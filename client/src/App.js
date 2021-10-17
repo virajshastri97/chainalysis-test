c68692f4ef4fa86389f95feb7947ff9685ab8cc7
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-17 16:55:01 -0400

    Add basic api call to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
