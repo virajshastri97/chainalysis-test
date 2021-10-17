704ffef9410b9c3a3108341aaddff1856d290620
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-17 16:17:01 -0400

    Add basic server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
