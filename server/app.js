81a0c3c31b0eb6fed0d2597e941dc85a7d77ca0a
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-18 15:44:48 -0400

    Add info for deployment

--- server/app.js
+++ server/app.js
@@ -40,8 +40,16 @@ setInterval(getCurrentBook, 600000);
 
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
   res.send(data);
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
