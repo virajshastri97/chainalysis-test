68e73ee72c870e726adebeb7b6714b2b80ff25ec
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-18 02:25:49 -0400

    Add title

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import CurrentBookContainer from './containers/bookContainer';
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <h1>Chainalysis BTC-ETH Exchange Rates</h1>
+      </div>
       <CurrentBookContainer />
     </div>
   );
