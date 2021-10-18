bc3b99565258465131741857dfdaefedba462107
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-18 02:23:55 -0400

    Add try catch around server api call

--- client/src/actions/Actions.js
+++ client/src/actions/Actions.js
@@ -7,14 +7,18 @@ export const getCurrentBook = () => async (dispatch) => {
   let btcBin = { "buy": 0, "sell": 0};
   let btcCnb = { "buy": 0, "sell": 0};
 
-  const book = await axios.get('/api');
+  try {
+    const book = await axios.get('/api');
 
-  book.data.forEach((item) => {
-    if (item.exchange === 'Binance' && item.coin === 'ETH') ethBin = { "buy": item.buy, "sell": item.sell };
-    else if (item.exchange === 'Coinbase' && item.coin === 'ETH') ethCnb = { "buy": item.buy, "sell": item.sell };
-    else if (item.exchange === 'Binance' && item.coin === 'BTC') btcBin = { "buy": item.buy, "sell": item.sell };
-    else btcCnb = { "buy": item.buy, "sell": item.sell };
-  });
+    book.data.forEach((item) => {
+      if (item.exchange === 'Binance' && item.coin === 'ETH') ethBin = { "buy": item.buy, "sell": item.sell };
+      else if (item.exchange === 'Coinbase' && item.coin === 'ETH') ethCnb = { "buy": item.buy, "sell": item.sell };
+      else if (item.exchange === 'Binance' && item.coin === 'BTC') btcBin = { "buy": item.buy, "sell": item.sell };
+      else btcCnb = { "buy": item.buy, "sell": item.sell };
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
   dispatch({
     type: GET_CURRENT_BOOK,
