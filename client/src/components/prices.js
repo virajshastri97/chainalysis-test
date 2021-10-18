ceb0b6cf562a00da7019fb0adfb8c557a57a765c
virajshastri97 <viraj.shastri@colostate.edu>
2021-10-18 02:24:21 -0400

    Minor changes

--- client/src/components/prices.js
+++ client/src/components/prices.js
@@ -1,7 +1,7 @@
 const Prices = (props) => (
-  <div>
-    <div className="buy">{props.prices.buy}</div>
-    <div className="sell">{props.prices.sell}</div>
+  <div className="rates">
+    <div className="buy">${props.prices.buy}</div>
+    <div className="sell">${props.prices.sell}</div>
   </div>
 );
 
